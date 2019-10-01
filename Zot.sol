d3b002500fbbfba9089971a6a9b4d75e183484ee
Sanjith <sanjith@pacbell.net>
2019-10-01 09:20:28 -0700

    added registered clubs and admin functins

--- Zot.sol
+++ Zot.sol
@@ -18,7 +18,8 @@ contract Zot {
     // basic events that can be invoked
     event transfer(address _from, address _to, uint transfer_value);
     event approval(address _owner, address _spender, uint transfer_value);
-    event new_club_created(address _owner, string club_name, string club_symbol);
+    event new_club_created(address _owner, string club_name, address club_address);
+    event new_admin_added(address _new_admin, address _approval_admin);
 
     // necessary mappings to keep track of zot amounts and owed amounts
     mapping (address => uint) public balances;
@@ -57,16 +58,27 @@ contract Zot {
         emit approval(_to, _from, num_zots);
     }
 
-    function set_new_club(address _new_club, address _owner, address _club_account) public payable {
+    //creates new club based on name, address, wallet, and owner
+    function set_new_club(string memory _new_club_name, address _new_club, address _owner, address _club_account) public payable {
         require(keccak256(abi.encodePacked(user_type[_owner])) == keccak256("student"), "type of address not permited to create new club");
         club_owners[_new_club] = _owner;
         club_members[_new_club] = [_owner];
         club_wallet[_new_club] = _club_account;
         balances[_club_account] = 0;
+        emit new_club_created(_owner, _new_club_name, _club_account);
     }
 
+    // registers new member into a club and pushes into the club member list
     function register_new_member(address _new_member, address _club_to_join) public payable{
         require(keccak256(abi.encode(user_type[_new_member])) == keccak256("member"), "Invalid member to join");
         club_members[_club_to_join].push(_new_member);
+    }
+
+    // adds new admin onto the contract, requires another admin to give approval
+    function add_new_admin(address _member_to_admin, address _approved_admin) public payable{
+        require(keccak256(abi.encode(user_type[_approved_admin])) == keccak256("admin"), "approved_admin not allowed to approve new users");
+        require(keccak256(abi.encode(user_type[_member_to_admin])) == keccak256("member"), "not possible based on types of users");
+        user_type[_member_to_admin] = "admin";
+        emit new_admin_added(_member_to_admin, _approved_admin);
     }
 }
